52bb64ec7e2ddc7b37708ba5bf89621215a56817
= <=>
2022-10-11 19:09:49 +0300

    add support to page click callback

--- src/features/paggination/Paggination.tsx
+++ src/features/paggination/Paggination.tsx
@@ -13,7 +13,7 @@ type Props = {
   itemsPerPage: number;
   totalItems: number;
   rangeSize?: number;
-  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+  onClick?: (e: React.MouseEvent<HTMLElement>, pageNum: number) => void;
 };
 export default function Paggination({
   itemsPerPage = 5,
@@ -26,48 +26,55 @@ export default function Paggination({
   const [currentPage, setCurrentPage] = useState(1);
   const startRef = useRef(0);
   const endRef = useRef(0);
-  const navigate = useNavigate()
-  
-  const setPageParam = (num:number)=>{
-    const searchParams = new URLSearchParams(location.search)
-    const page = (num) + ""
-    searchParams.set("page", page)
-    const uri = location.pathname + "?" + searchParams.toString()
-    navigate(uri)
-  }
-  
+  const navigate = useNavigate();
+
+  const setPageParam = (num: number) => {
+    const searchParams = new URLSearchParams(location.search);
+    const page = num + "";
+    searchParams.set("page", page);
+    const uri = location.pathname + "?" + searchParams.toString();
+    navigate(uri);
+  };
+
   useEffect(() => {
     console.log("useEffect, location is:", location);
     const page = Number(new URLSearchParams(location.search).get("page") || 1);
-    if (page <=  (Math.floor( rangeSize / 2) + 1)){
-      startRef.current = 1
-      endRef.current = rangeSize
-    }else{
-      const totalPages = Math.ceil(totalItems /itemsPerPage) ;
-      if(totalPages>=(page + Math.floor(rangeSize / 2))){
-        startRef.current = page - Math.ceil(rangeSize / 2) + 1
-        endRef.current = page + Math.floor(rangeSize / 2)
+    if (page <= Math.floor(rangeSize / 2) + 1) {
+      startRef.current = 1;
+      endRef.current = rangeSize;
+    } else {
+      const totalPages = Math.ceil(totalItems / itemsPerPage);
+      if (totalPages >= page + Math.floor(rangeSize / 2)) {
+        startRef.current = page - Math.ceil(rangeSize / 2) + 1;
+        endRef.current = page + Math.floor(rangeSize / 2);
       }
     }
-    
-    setCurrentPage(page)
+
+    setCurrentPage(page);
     setRangeSlice(range(startRef.current, endRef.current));
   }, [setRangeSlice, rangeSize, itemsPerPage, location, totalItems]);
 
   const adjacentPage = (num: number) => {
-    setPageParam(currentPage + num)
+    setPageParam(num);
   };
 
   const handleClick = (num: number) => {
-    setPageParam(num)
+    setPageParam(num);
   };
   const totalPages = Math.ceil(totalItems / itemsPerPage);
-  
+
   return (
     <div className="flex items-center justify-center">
       <div className="min-w-[100px]">
         {currentPage > 1 && (
-          <div className="mx-5 cursor-pointer" onClick={() => adjacentPage(-1)}>
+          <div
+            className="mx-5 cursor-pointer"
+            onClick={(e) => {
+              const page = currentPage - 1;
+              adjacentPage(page);
+              onClick && onClick(e, page);
+            }}
+          >
             Previous
           </div>
         )}
@@ -78,7 +85,10 @@ export default function Paggination({
             <div
               key={i}
               className="padding mx-1"
-              onClick={() => handleClick(num)}
+              onClick={(e) => {
+                handleClick(num);
+                onClick && onClick(e, num);
+              }}
             >
               {currentPage === num ? (
                 <div className="text-slate-400 bold">{num}</div>
@@ -91,7 +101,14 @@ export default function Paggination({
       </div>
       <div className="min-w-[100px]">
         {currentPage < totalPages && (
-          <div className="mx-5 cursor-pointer" onClick={() => adjacentPage(1)}>
+          <div
+            className="mx-5 cursor-pointer"
+            onClick={(e) => {
+              const page = currentPage + 1;
+              adjacentPage(page);
+              onClick && onClick(e, page);
+            }}
+          >
             Next
           </div>
         )}
